b1c7d7049fe4213c581abc76260d809cce1b236c
Lakshmi096 <lakshmirk.096@gmail.com>
2020-08-02 17:04:04 +0530

    Updates

---
+++ precache-manifest.98d59acf58bbbeba28d5eb46221308c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354b5c8d1d09404c0ae17ea3a53a0517",
+    "url": "/react-tinder-clone/index.html"
+  },
+  {
+    "revision": "694196c96155aa774c1c",
+    "url": "/react-tinder-clone/static/css/main.f6f309ae.chunk.css"
+  },
+  {
+    "revision": "fbd000598e7098384106",
+    "url": "/react-tinder-clone/static/js/2.b2bb7356.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/react-tinder-clone/static/js/2.b2bb7356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694196c96155aa774c1c",
+    "url": "/react-tinder-clone/static/js/main.c701fba0.chunk.js"
+  },
+  {
+    "revision": "71908ed0361d1509161d",
+    "url": "/react-tinder-clone/static/js/runtime-main.5e6eecdc.js"
+  }
+]);
